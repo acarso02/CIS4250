f5d87cf15cd0b70e1ca370212c6a2b1f8230fba8
Ishan <iruchal@uoguelph.ca>
2022-02-17 15:36:06 -0500

    Initial commit
    Initial commit

---
+++ app/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import {StyleSheet, View, Button, Text, TouchableWithoutFeedback} from "react-native"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignIn from './SignInScreen';
+import Register from './Register';
+
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.background}>
+   <View style={styles.loginButton}> 
+        <Button title = "Login" color="black" onPress={() =>navigation.navigate(SignIn)}> </Button>
+    </View>
+     <View style={styles.registerButton}> 
+         <Button title = "Register" color="black" onPress={() =>navigation.navigate(Register)}> </Button>
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  background: {
+    flex: 1,
+    backgroundColor: 'seashell',
+    justifyContent: 'flex-end'
+  },
+
+  loginButton: {
+    width: '100%',
+    height: 80,
+  },
+
+    registerButton: {
+    width: '100%',
+    height: 80,
+  },
+  
+});
+
+export default HomeScreen;
---
+++ app/Register.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import reactDom from 'react-dom';
+import {StyleSheet, View, Button, Text, TextInput, StatusBar, TouchableOpacity} from "react-native"
+import HomeScreen from './HomeScreen';
+
+const Register = ({navigation}) => {
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+  return (
+    <View style={styles.background}>
+         <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Confirm Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+
+   <View style={styles.loginButton}> 
+        <Button title = "Register" color="black" onPress={() =>navigation.navigate(HomeScreen)}> </Button>
+    </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  background: {
+    flex: 1,
+    backgroundColor: 'seashell',
+    justifyContent: 'flex-end'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+  image: {
+    marginBottom: 40,
+  },
+ 
+  inputView: {
+    backgroundColor: "lavender",
+    borderRadius: 30,
+    width: "100%",
+    height: 45,
+    marginBottom: 25,
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    padding: 10,
+    alignItems: "center",
+  },
+ 
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+    alignItems: "center",
+  },
+
+    loginButton: {
+    width: '100%',
+    height: 100,
+  },
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+});
+
+export default Register;
